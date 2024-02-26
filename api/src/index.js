82e7b16e352318d47f7b63b0c3af752d0e715aaf
Pal Bijewar <106950125+palbijewar@users.noreply.github.com>
2024-02-26 15:19:52 +0530

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://graceful-boa-belt.cyclic.app',
   credentials: true, 
 }));
 
