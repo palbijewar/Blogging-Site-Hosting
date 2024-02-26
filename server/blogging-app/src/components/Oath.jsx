377ba3ae01d21ca16e7c608d9894682d066d3096
Pal Bijewar <pal.bijewar@gmail.com>
2024-02-26 12:42:58 +0530

    error correction

--- server/blogging-app/src/components/Oath.jsx
+++ server/blogging-app/src/components/Oath.jsx
@@ -16,7 +16,7 @@ export default function OAuth() {
         provider.setCustomParameters({ prompt: 'select_account' })
         try {
             const resultsFromGoogle = await signInWithPopup(auth, provider)
-            const res = await fetch('${baseUrl}/api/users/google', {
+            const res = await fetch(`${baseUrl}/api/users/google`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 credentials: 'include',
